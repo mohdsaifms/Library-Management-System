163d126a28a9ca73c1f0be83731e751f70a96338
mohdsaifms <155008010+mohdsaifms@users.noreply.github.com>
2023-12-28 00:52:41 +0530

    Add files via upload

---
+++ Library.java
@@ -0,0 +1,32 @@
+package library_ManSystem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Library {
+    private List<Book> books;
+
+    public Library() {
+        books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void displayBooks() {
+        for (Book book : books) {
+            System.out.println(book);
+        }
+    }
+
+    public Book findBookById(int bookId) {
+        for (Book book : books) {
+            if (book.getBookId() == bookId) {
+                return book;
+            }
+        }
+        return null;
+    }
+}
+
